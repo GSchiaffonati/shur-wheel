fd97b1acdb92d62787b062bc9233c88acf29ebe9
Gianmarco Schiaffonati <g.schiaffonati@gmail.com>
2016-07-08 16:54:06 +0100

    fancy is working!

--- src/fancy/fancy.ino
+++ src/fancy/fancy.ino
@@ -14,7 +14,7 @@ FASTLED_USING_NAMESPACE
 #define NUM_LEDS 15
 CRGB leds[NUM_LEDS];
 
-#define BRIGHTNESS  10
+#define BRIGHTNESS  100
 #define FRAMES_PER_SECOND 120
 
 Adafruit_AlphaNum4 alpha4 = Adafruit_AlphaNum4();
@@ -45,65 +45,30 @@ void setup(){
   pinMode(SegE, OUTPUT);
   pinMode(SegF, OUTPUT);
   pinMode(SegG, OUTPUT);
-
-  int i;
-  for(i=0;i<NUM_LEDS;i++){
-    leds[i].red = 0;//set to off
-    leds[i].green = 0;
-    leds[i].blue = 0;
-    FastLED.show();
-    }
-  
-  
 }
 
 void loop(){
-  int ledState = 0;
   Serial.println("main start");
-  //FastLED.show();
-  //FastLED.delay(1000 / FRAMES_PER_SECOND);
   alphaDis();
   segR();
-  ledState = digitalRead(Button1);
-  if(ledState == HIGH){
-    led();
-  }
+  
 }
 
 void alphaDis(){
-  alpha4.writeDigitAscii(0, ' ');
-  alpha4.writeDigitAscii(1, 'S');
-  alpha4.writeDigitAscii(2, 'H');
-  alpha4.writeDigitAscii(3, 'U');
+  alpha4.writeDigitAscii(0, 'S');
+  alpha4.writeDigitAscii(1, 'H');
+  alpha4.writeDigitAscii(2, 'U');
+  alpha4.writeDigitAscii(3, ' ');
   alpha4.writeDisplay();
 }
 
 void segR(){
-  digitalWrite(SegA, LOW);
-  digitalWrite(SegB, HIGH);
-  digitalWrite(SegC, HIGH);
-  digitalWrite(SegD, HIGH);
-  digitalWrite(SegE, LOW);
-  digitalWrite(SegF, LOW);
-  digitalWrite(SegG, HIGH);
-}
-
-void led(){
-  int i = 0;
-  for(i;i<4;i++){
-    leds[i].green = 20;
-    FastLED.show();
-    delay(250);
-    }
-  for(i;i<8;i++){
-    leds[i].red = 20;
-    FastLED.show();
-    delay(250);
-  }
-  for(i;i<12;i++){
-    leds[i].blue = 20;
-    FastLED.show();
-    delay(250);
-  }
+  digitalWrite(SegA, HIGH);
+  digitalWrite(SegB, LOW);
+  digitalWrite(SegC, LOW);
+  digitalWrite(SegD, LOW);
+  digitalWrite(SegE, HIGH);
+  digitalWrite(SegF, HIGH);
+  digitalWrite(SegG, LOW);
 }
 
